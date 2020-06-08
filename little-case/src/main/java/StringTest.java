3b45c0b65f18f81db7bbc9e3f688831e5795afca
daiyunjiao <daiyunjiao@xinchao.com>
2020-06-08 12:02:32 +0800

    save

--- little-case/src/main/java/StringTest.java
+++ little-case/src/main/java/StringTest.java
@@ -1,12 +1,30 @@
+import java.util.Stack;
+
 public class StringTest {
     public static void main(String[] args) {
         System.out.println(reverse("  ustax"));
         System.out.println(reverse("ABCd"));
+        System.out.println(reverse2("ABCd"));
     }
 
     public static String reverse(String s) {
         if (s != null) {
             return new StringBuilder(s).reverse().toString();
+        }
+        return null;
+    }
+
+    public static String reverse2(String s) {
+        if (s != null) {
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < s.length(); i++) {
+                stack.push(s.charAt(i));
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+            while (!stack.empty()) {
+                stringBuilder.append(stack.pop());
+            }
+            return stringBuilder.toString();
         }
         return null;
     }
